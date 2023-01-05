758d79a85c2a248906cd9cccbe25a347a37c150e
John <John.Arscott@hotmail.com>
2023-01-05 22:35:16 +0000

    add update cart functionality to qty

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from products.models import Product
 
 
 def view_cart(request):
@@ -18,3 +19,16 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
     print(request.session['cart'])
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
--- products/models.py
+++ products/models.py
@@ -12,6 +12,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=255)
     description = models.TextField()
+    color = models.CharField(default='white', max_length=255)
     sku = models.CharField(max_length=255, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1020, null=True, blank=True)
