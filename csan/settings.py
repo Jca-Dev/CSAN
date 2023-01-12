a5903fb43e8630db9d147bc9802aaf4635edff68
John <John.Arscott@hotmail.com>
2023-01-12 01:38:21 +0000

    secure secret key and change debug value

--- csan/settings.py
+++ csan/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lwx)a(a!a3)7uh-1v!=1s7ggupk!n-w$)-hx$#!e=ja!$%218n'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '', 'csan.herokuapp.com']
 
