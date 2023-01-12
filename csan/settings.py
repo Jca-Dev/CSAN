edc78802485f417cab0208459206afcde8b6e388
John <John.Arscott@hotmail.com>
2023-01-12 17:10:31 +0000

    remove empty string from local hosts

--- csan/settings.py
+++ csan/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '', 'csan.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'csan.herokuapp.com']
 
 
 # Application definition
