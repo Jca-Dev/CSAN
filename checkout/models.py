9d4d7443b8dcafde2b63164a1925e2b5241f0462
John <John.Arscott@hotmail.com>
2023-01-16 18:00:12 +0000

    refactor css for mobile view

--- checkout/models.py
+++ checkout/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django_countries.fields import CountryField
 from products.models import Product
 from profiles.models import UserProfile
+from django.core.validators import MaxValueValidator
 
 
 class Order(models.Model):
@@ -13,7 +14,8 @@ class Order(models.Model):
                                      null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    phone_number = models.IntegerField(null=False, blank=False,
+                                       validators=[MaxValueValidator(9999999999)])
     country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
